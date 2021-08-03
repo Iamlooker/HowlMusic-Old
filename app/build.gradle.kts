6fe1be614b11232c7528002aee77ab6358bc6243
Mohit <mohit2002ss@gmail.com>
2021-08-03 22:14:04 +0530

    Removed Room DB

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,11 +73,7 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.4.0-alpha05")
 
     //Palette
-    implementation("androidx.palette:palette-ktx:1.0.0")
-
-    //Room
-    implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
-    annotationProcessor("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
+    implementation("androidx.palette:palette-ktx:1.0.0"))
 
     //UI
     implementation("androidx.compose.ui:ui:${rootProject.extra["compose_version"]}")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 buildscript {
+    
     val compose_version by extra("1.0.0")
     val lifecycle_version by extra("2.3.1")
-    val room_version by extra("2.3.0")
+
     repositories {
         google()
         mavenCentral()
