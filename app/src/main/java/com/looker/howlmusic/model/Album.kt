09cddee9bdea99e6e5e22bdb92d43d635f14dac0
Mohit <mohit2002ss@gmail.com>
2021-08-03 22:13:27 +0530

    Remove unused imports

--- app/src/main/java/com/looker/howlmusic/model/Album.kt
+++ app/src/main/java/com/looker/howlmusic/model/Album.kt
@@ -1,11 +1,10 @@
 package com.looker.howlmusic.model
 
-import android.graphics.Bitmap
 import android.net.Uri
 
 data class Album(
     val albumName: String,
     val artistName: String,
     val albumArtUri: Uri? = null,
-    val albumId: Long
+    val albumId: Long,
 )
