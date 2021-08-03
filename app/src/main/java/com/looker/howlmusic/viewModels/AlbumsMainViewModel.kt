725f0947e5dd17c135e2a044b06c7d9299a818a8
Mohit <mohit2002ss@gmail.com>
2021-08-03 22:19:29 +0530

    -

--- app/src/main/java/com/looker/howlmusic/viewModels/AlbumsMainViewModel.kt
+++ app/src/main/java/com/looker/howlmusic/viewModels/AlbumsMainViewModel.kt
@@ -1,8 +1,7 @@
 package com.looker.howlmusic.viewModels
 
 import androidx.lifecycle.ViewModel
-import com.looker.howlmusic.data.AlbumsData
 
 class AlbumsMainViewModel : ViewModel() {
-    // setup room database
+
 }
