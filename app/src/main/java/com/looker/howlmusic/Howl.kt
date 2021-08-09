488d98f418a3a5f3329a1f90ae0ab1345b65cdd2
Mohit <mohit2002ss@gmail.com>
2021-08-09 22:14:23 +0530

    Fixed Padding for 3 button Navigation

--- app/src/main/java/com/looker/howlmusic/Howl.kt
+++ app/src/main/java/com/looker/howlmusic/Howl.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.insets.navigationBarsPadding
 import com.looker.howlmusic.ui.components.BottomNavigation
 import com.looker.howlmusic.ui.home.HomeSections
 import com.looker.howlmusic.ui.theme.HowlMusicTheme
@@ -21,7 +22,8 @@ fun HowlApp() {
         Scaffold(
             bottomBar = {
                 BottomNavigation(navController = navController, items = items)
-            }
+            },
+            modifier = Modifier.navigationBarsPadding()
         ) { innerPadding ->
             Box(modifier = Modifier.padding(innerPadding)) {
                 HowlNavGraph(navController)
