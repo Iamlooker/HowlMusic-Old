e7fb0b253694131eacd5cbf0f8362605ca4ec8d4
Mohit <mohit2002ss@gmail.com>
2021-08-13 22:54:50 +0530

    Added New Back press navigation handling

--- app/src/main/java/com/looker/howlmusic/ui/components/BottomNavigationSurface.kt
+++ app/src/main/java/com/looker/howlmusic/ui/components/BottomNavigationSurface.kt
@@ -1,15 +1,14 @@
 package com.looker.howlmusic.ui.components
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.core.net.toUri
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph
 import androidx.navigation.compose.currentBackStackEntryAsState
-import com.looker.howlmusic.model.Song
 import com.looker.howlmusic.ui.home.HomeSections
 
 @Composable
@@ -21,39 +20,38 @@ fun BottomNavigation(navController: NavController, items: Array<HomeSections>) {
     val sections = remember { HomeSections.values() }
     val routes = remember { sections.map { it.route } }
 
-    Column {
-        MiniPlayer(
-            song = Song(
-                songUri = "content://media/external/audio/media/809".toUri(),
-                albumId = 1472833616896716278,
-                songName = "PLAYING WITH FIRE",
-                artistName = "BLACKPINK",
-                songDurationMillis = 197329
-            )
-        )
-        if (currentRoute in routes) {
-            BottomNavigation(
-                backgroundColor = MaterialTheme.colors.background,
-                contentColor = MaterialTheme.colors.primary,
-            ) {
-                val currentDestination = navBackStackEntry?.destination
-                items.forEach { screen ->
-                    BottomNavigationItem(
-                        icon = { Icon(screen.icon, contentDescription = null) },
-                        label = { Text(screen.title) },
-                        selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
-                        alwaysShowLabel = false,
-                        selectedContentColor = MaterialTheme.colors.primary,
-                        unselectedContentColor = MaterialTheme.colors.surface,
-                        onClick = {
-                            navController.navigate(screen.route) {
-                                launchSingleTop = true
-                                restoreState = true
+    if (currentRoute in routes) {
+        BottomNavigation(
+            backgroundColor = MaterialTheme.colors.background,
+            contentColor = MaterialTheme.colors.primary,
+        ) {
+            val currentDestination = navBackStackEntry?.destination
+            items.forEach { screen ->
+                BottomNavigationItem(
+                    icon = { Icon(screen.icon, contentDescription = null) },
+                    label = { Text(screen.title) },
+                    selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                    alwaysShowLabel = false,
+                    selectedContentColor = MaterialTheme.colors.primary,
+                    unselectedContentColor = MaterialTheme.colors.surface,
+                    onClick = {
+                        navController.navigate(screen.route) {
+                            launchSingleTop = true
+                            restoreState = true
+                            popUpTo(findStartDestination(navController.graph).id) {
+                                saveState = true
                             }
                         }
-                    )
-                }
+                    }
+                )
             }
         }
     }
+}
+
+private val NavGraph.startDestination: NavDestination?
+    get() = findNode(startDestinationId)
+
+private tailrec fun findStartDestination(graph: NavDestination): NavDestination {
+    return if (graph is NavGraph) findStartDestination(graph.startDestination!!) else graph
 }
