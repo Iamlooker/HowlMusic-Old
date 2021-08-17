0afb5f0395bc4fd1bc0faf85148efe79c306ab46
Mohit <mohit2002ss@gmail.com>
2021-08-17 16:36:38 +0530

    Get Better Colors from Colors.kt

--- app/src/main/java/com/looker/howlmusic/utils/Colors.kt
+++ app/src/main/java/com/looker/howlmusic/utils/Colors.kt
@@ -10,10 +10,7 @@ import androidx.core.graphics.drawable.toBitmap
 import androidx.palette.graphics.Palette
 import coil.Coil
 import coil.request.ImageRequest
-import coil.request.SuccessResult
 import coil.size.Scale
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
 @Composable
 fun rememberDominantColorState(
@@ -21,18 +18,16 @@ fun rememberDominantColorState(
     defaultColor: Color = MaterialTheme.colors.surface,
     defaultOnColor: Color = MaterialTheme.colors.onSurface,
     cacheSize: Int = 12,
-    isColorValid: (Color) -> Boolean = { true },
 ): DominantColorState = remember {
-    DominantColorState(context, defaultColor, defaultOnColor, cacheSize, isColorValid)
+    DominantColorState(context, defaultColor, defaultOnColor, cacheSize)
 }
 
 @Stable
 class DominantColorState(
     private val context: Context,
-    private val defaultColor: Color,
-    private val defaultOnColor: Color,
+    defaultColor: Color,
+    defaultOnColor: Color,
     cacheSize: Int = 50,
-    private val isColorValid: (Color) -> Boolean = { true },
 ) {
     var color by mutableStateOf(defaultColor)
         private set
@@ -46,57 +41,46 @@ class DominantColorState(
 
     suspend fun updateColorsFromImageUrl(url: String) {
         val result = calculateDominantColor(url)
-        color = result?.color ?: defaultColor
-        onColor = result?.onColor ?: defaultOnColor
+        color = result.color
+        onColor = result.onColor
     }
 
-    private suspend fun calculateDominantColor(url: String): DominantColors? {
-        return cache?.get(url)
-            ?: calculateSwatchesInImage(context, url)
-                .sortedByDescending { swatch -> swatch.rgb }
-                .firstOrNull { swatch -> isColorValid(Color(swatch.rgb)) }
-                ?.let { swatch ->
-                    DominantColors(
-                        color = Color(swatch.rgb),
-                        onColor = Color(swatch.bodyTextColor).copy(alpha = 0.4f)
-                    )
-                }
-                ?.also { result -> cache?.put(url, result) }
-    }
-
-    fun reset() {
-        color = defaultColor
-        onColor = defaultColor
+    private suspend fun calculateDominantColor(url: String): DominantColors {
+        return cache?.get(url) ?: DominantColors(
+            color = calculatePrimaryColorInImage(context, url),
+            onColor = calculatePrimaryColorInImage(context, url).copy(alpha = 0.4f)
+        )
+            .also { result -> cache?.put(url, result) }
     }
 }
 
 @Immutable
 private data class DominantColors(val color: Color, val onColor: Color)
 
-private suspend fun calculateSwatchesInImage(
+
+private suspend fun calculatePrimaryColorInImage(
     context: Context,
     imageUrl: String,
-): List<Palette.Swatch> {
+): Color {
     val r = ImageRequest.Builder(context)
         .data(imageUrl)
         .size(128).scale(Scale.FILL)
         .allowHardware(false)
         .build()
 
-    val bitmap = when (val result = Coil.execute(r)) {
-        is SuccessResult -> result.drawable.toBitmap()
-        else -> null
-    }
+    val result = Coil.execute(r)
 
-    return bitmap?.let {
-        withContext(Dispatchers.Default) {
-            val palette = Palette.Builder(bitmap)
-                .resizeBitmapArea(0)
-                .clearFilters()
-                .maximumColorCount(8)
-                .generate()
+    val bitmap = result.drawable?.toBitmap()
 
-            palette.swatches
-        }
-    } ?: emptyList()
+    val swatch = Palette.Builder(bitmap!!)
+        .resizeBitmapArea(0)
+        .clearFilters()
+        .maximumColorCount(8)
+        .generate()
+
+    val vibrant = swatch.getVibrantColor(0)
+    val dominant = swatch.getDominantColor(0)
+
+    return if (vibrant == 0) Color(dominant)
+    else Color(vibrant)
 }
